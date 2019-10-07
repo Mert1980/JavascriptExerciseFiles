9fd280c9db525569ea2f2962ea633877254b803f
Mert Demirok <mertdemirok80@gmail.com>
2019-10-07 20:42:57 +0200

    calling string, if-else exercises are uploaded

---
+++ 01JS_BASICS/else_if_staments.js
@@ -0,0 +1,38 @@
+var weather = "sunny";
+
+if (weather === "snow") {
+  console.log("Bring a coat.");
+} else if (weather === "rain") {
+  console.log("Bring a rain jacket.");
+} else {
+  console.log("Wear what you have on.");
+}
+
+var money = 100.50;
+var price = 100.50;
+
+if (money > price) {
+  console.log("You paid extra, here's your change.");
+} else if (money === price) {
+  console.log("You paid the exact amount, have a nice day!");
+} else {
+  console.log("That's not enough, you still owe me money.");
+}
+
+var runner = "Kendyll";
+var position = 2;
+var medal;
+
+if(position === 1) {
+  medal = "gold";
+} else if(position === 2) {
+  medal = "silver";
+} else if(position === 3) {
+  medal = "bronze";
+} else {
+  medal = "pat on the back";
+}
+
+console.log(runner + " received a " + medal + " medal.");
+
+
---
+++ 01JS_BASICS/if_else_statements.js
@@ -0,0 +1,8 @@
+var price="15.00";//price of hammer
+var money="14.00";//money I have
+if (money>=price){
+    console.log("buy the hammer");
+}
+    else{
+        console.log("You do not have enough money");
+    }
